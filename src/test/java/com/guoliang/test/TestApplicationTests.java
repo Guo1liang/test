d4b21608261dac7484e177e0601f935f49db5699
Guoliang <951670485@qq.com>
2021-10-09 15:58:47 +0800

    guoliang commit

--- src/test/java/com/guoliang/test/TestApplicationTests.java
+++ src/test/java/com/guoliang/test/TestApplicationTests.java
@@ -9,7 +9,10 @@ class TestApplicationTests {
     @Test
     void contextLoads() throws Exception {
         System.out.println(1);
-        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
     }
 
 }
