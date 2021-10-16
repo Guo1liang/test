e0460bf5346e03cc7cbed096f6e2a3db3cf6720b
Guoliang <951670485@qq.com>
2021-10-16 22:21:10 +0800

    静态资源的结构与规则

--- src/main/java/com/guoliang/test/controller/test.java
+++ src/main/java/com/guoliang/test/controller/test.java
@@ -2,21 +2,17 @@ package com.guoliang.test.controller;
 
 
 import com.guoliang.test.entity.Rest;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.Date;
 
 @RestController
-@CrossOrigin
 public class test {
 
     @GetMapping("/test")
-    public Rest test() throws IOException {
-
-        return new Rest("200", new Date(), "ok" );
+    public Rest<Date> test() {
+        return new Rest<>("200", new Date(), "ok");
     }
 
 }
